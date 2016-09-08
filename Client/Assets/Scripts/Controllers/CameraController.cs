dac956bc88205488f9702a4457f1411e6565b8e8
Alex Zable <alzabled@gmail.com>
2016-09-09 08:53:37 +1000

    Major client updates (snake code refactoring + thickness parametizations)

---
+++ Client/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public SnakeState snakeToTrack;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = snakeToTrack.head.transform.position + Vector3.back * 20.0f;
+        this.camera.orthographicSize = snakeToTrack.GetSnakeThickness() * 10.0f;
+	}
+}
--- Client/Assets/Scripts/Views/SnakeMeshGenerator.cs
+++ Client/Assets/Scripts/Views/SnakeMeshGenerator.cs
@@ -6,33 +6,48 @@ using System.Linq;
 public class SnakeMeshGenerator : MonoBehaviour {
 
     public Texture snakeSkin;
-    public float snakeThickness;
+    public float snakeRadius;
     public float snakeVertexDensity;
+    public float snakeLength;
+    public float snakeTailStartLength;
 
     private SnakeState snake;
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         this.snake = this.GetComponent<SnakeState>();
         this.meshFilter = this.gameObject.AddComponent<MeshFilter>();
         this.meshFilter.mesh = new Mesh();
         this.meshRenderer = this.gameObject.AddComponent<MeshRenderer>();
-
-        this.meshRenderer.material.mainTexture = snakeSkin;
+        
         this.meshRenderer.material.shader = Shader.Find("Unlit/TexShader");
+    }
+
+    void Update ()
+    {
+        this.snakeTailStartLength = this.snakeLength - 5.0f;
+
+        this.snakeLength = this.snake.GetSnakeLength();
+        this.snakeRadius = this.snake.GetSnakeThickness() / 2.0f;
+        this.snakeVertexDensity = 3.0f / this.snakeRadius;
+
+        this.snakeSkin = Resources.Load<Texture>("SnakeSkin" + this.snake.snakeSkinID);
+        this.meshRenderer.material.mainTexture = snakeSkin;
     }
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        int backboneLength = (int)(this.snake.snakeLength * this.snakeVertexDensity);
+        int backboneLength = (int)(this.snakeLength * this.snakeVertexDensity);
+        if (backboneLength <= 0)
+            return;
         Vector3[] backbone = new Vector3[backboneLength];
         
         float distance = 0.0f;
         for (int i = 0; i < backboneLength; i++)
         {
-            distance = ((float)i / backboneLength) * this.snake.snakeLength;
+            distance = ((float)i / backboneLength) * this.snakeLength;
             backbone[i] = this.snake.CalcBackboneParametizedPosition(distance);
         }
 
@@ -44,8 +59,8 @@ public class SnakeMeshGenerator : MonoBehaviour {
         distance = 0.0f;
         for (int i = 0; i < backboneLength; i++)
         {
-            uvs[i * 2] = new Vector2(1.0f, distance / (snakeThickness * 2.0f));
-            uvs[i * 2 + 1] = new Vector2(0.0f, distance / (snakeThickness * 2.0f));
+            uvs[i * 2] = new Vector2(1.0f, distance / (snakeRadius * 2.0f));
+            uvs[i * 2 + 1] = new Vector2(0.0f, distance / (snakeRadius * 2.0f));
 
             float distanceDelta = 0.0f;
 
@@ -70,14 +85,14 @@ public class SnakeMeshGenerator : MonoBehaviour {
             norm.Normalize();
 
             float fct = Mathf.Clamp((backboneLength - i - 1) / (backboneLength * 0.1f), 0.0f, 1.0f);
-            if (distance < snakeThickness * 2.0f)
+            if (distance < snakeRadius * 2.0f)
             {
                 // Head taper
-                fct = Mathf.Clamp((distance / (snakeThickness * 2.0f)) + 0.3f, 0.0f, 1.0f);
+                fct = Mathf.Clamp((distance / (snakeRadius * 2.0f)) + 0.3f, 0.0f, 1.0f);
             }
 
-            vertices[i * 2] = backbone[i] + norm * snakeThickness * fct + Vector3.forward * distance;
-            vertices[i * 2 + 1] = backbone[i] - norm * snakeThickness * fct + Vector3.forward * distance;
+            vertices[i * 2] = backbone[i] + norm * snakeRadius * fct + Vector3.forward * distance;
+            vertices[i * 2 + 1] = backbone[i] - norm * snakeRadius * fct + Vector3.forward * distance;
 
             distance += distanceDelta;
         }
@@ -92,10 +107,13 @@ public class SnakeMeshGenerator : MonoBehaviour {
             triangles[(i * 6) + 4] = (i + 1) * 2;
             triangles[(i * 6) + 5] = ((i + 1) * 2) + 1;
         }
+
+        //Debug.Log(vertices.Length);
         
         this.meshFilter.mesh.vertices = vertices;
         this.meshFilter.mesh.uv = uvs;
         this.meshFilter.mesh.triangles = triangles;
         this.meshFilter.mesh.UploadMeshData(false);
+        this.meshFilter.mesh.RecalculateBounds();
     }
 }
