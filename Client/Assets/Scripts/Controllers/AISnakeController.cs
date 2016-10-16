b6301258f4b0b2374d4cef585a436fd488f9911b
Alex Zable <alzabled@gmail.com>
2016-10-16 17:08:20 +1100

    Enhance snake AI, populate local scoreboard with random snake names

--- Client/Assets/Scripts/Controllers/AISnakeController.cs
+++ Client/Assets/Scripts/Controllers/AISnakeController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class AISnakeController : MonoBehaviour {
 	public SnakeState snake;
+    public float circleCastDistance = 4.0f;
 
 	private GameWorld gameWorld;
 	private Vector2 targetPoint;
@@ -20,7 +21,31 @@ public class AISnakeController : MonoBehaviour {
 
 	void Update () 
 	{
-		if (Vector2.Distance(snake.transform.position, targetPoint) < 1.0f || Random.Range(0, 500) == 0)
+        Vector2 lookDir = targetPoint - (Vector2)this.transform.position;
+        RaycastHit2D[] circleHits = Physics2D.CircleCastAll(this.transform.position, this.circleCastDistance, lookDir);
+
+        Vector2 hitPointAvg = Vector2.zero;
+        int hitPointCount = 0;
+        foreach (RaycastHit2D circleHit in circleHits)
+        {
+            if (circleHit.collider.gameObject.tag == "SnakeBody" &&
+                circleHit.collider.gameObject.GetComponent<SnakeStateReference>().snakeState != this.snake)
+            {
+                hitPointAvg += circleHit.point;
+                hitPointCount++;
+                break;
+            }
+        }
+
+        hitPointAvg /= hitPointCount;
+        targetPoint = (Vector2)this.transform.position + (((Vector2)this.transform.position - hitPointAvg).normalized * 5.0f);
+        if (targetPoint.magnitude >= this.gameWorld.worldRadius)
+        {
+            targetPoint = RandomTargetPoint();
+        }
+
+
+        if (Vector2.Distance(snake.transform.position, targetPoint) < 1.0f || Random.Range(0, 500) == 0)
 		{
 			targetPoint = RandomTargetPoint();
 		}
