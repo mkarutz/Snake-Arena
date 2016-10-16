43f6f9b88502b98adf2d6db9a62af3f27eea513d
Alex Zable <alzabled@gmail.com>
2016-10-17 10:34:35 +1100

    Scrollbar is now not shit

---
+++ Client/Assets/SetDefaultScrollBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetDefaultScrollBar : MonoBehaviour {
+
+    public ScrollRect scrollRect;
+
+    // Use this for initialization
+    void Awake () {
+    }
+
+    void Update()
+    {
+    }
+
+    void OnEnable()
+    {
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 1.0f;
+        Canvas.ForceUpdateCanvases();
+        scrollRect.velocity = new Vector2(0f, -1000f);
+    }
+}
