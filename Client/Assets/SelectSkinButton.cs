a140bd8044d85723867fd183e325efc966bbd22e
Alex Zable <alzabled@gmail.com>
2016-10-18 03:05:54 +1100

    Skin select working

---
+++ Client/Assets/SelectSkinButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectSkinButton : MonoBehaviour {
+
+    public int skinID;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void OnButtonPress () {
+        PlayerProfile.Instance().Skin = skinID;
+	}
+
+    void Update()
+    {
+        if (PlayerProfile.Instance().Skin == skinID)
+            this.gameObject.GetComponent<EnableDisableButton>().DisableButton();
+        else
+            this.gameObject.GetComponent<EnableDisableButton>().EnableButton();
+    }
+}
