47e2d49a732f10f2fe80ad6360a2ae4f1f42d40f
Malcolm Karutz <malcolm_karutz@live.com>
2016-10-21 14:32:31 +1100

    better network handling

--- Client/Assets/Scripts/Misc/GoToSinglePlayerScene.cs
+++ Client/Assets/Scripts/Misc/GoToSinglePlayerScene.cs
@@ -6,6 +6,6 @@ public class GoToSinglePlayerScene : MonoBehaviour
 {
     public void LoadSinglePlayerScene()
     {
-        SceneManager.LoadScene("InGameSingleplayer");
+		SceneManager.LoadSceneAsync("InGameSingleplayer");
     }
 }
